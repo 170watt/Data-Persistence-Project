1cedc0fea6fa64eaca7e9fa3505c6f0fe79e77e2
170watt <170watt@gmail.com>
2026-05-08 13:01:45 -0400

    Update02
    added a Text (BestScoreText) in Menu scene that reads the stored best player and score. If nothing stored, Just Best Score: none. In addition, if there is a name stored, prefill the InputField with that name

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -2,22 +2,42 @@ using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class MenuUIHandler : MonoBehaviour
 {
 
     public TMP_InputField NameInput; // NEW Drag your InputField here
+    public Text BestScoreText; // Add this for the Menu high score display
 
-    private void Start()
+    private void Start() //NEW
     {
+        // 1. Check if we have persistent data available
+        if (MainDataPersistence.Instance != null)
+        {
+            // 2. Prefill the InputField if a name was previously saved
+            if (!string.IsNullOrEmpty(MainDataPersistence.Instance.BestPlayerName))
+            {
+                NameInput.text = MainDataPersistence.Instance.BestPlayerName;
+            }
 
+            // 3. Update the High Score text
+            if (MainDataPersistence.Instance.HighScore > 0)
+            {
+                BestScoreText.text = $"Best Score: {MainDataPersistence.Instance.BestPlayerName} : {MainDataPersistence.Instance.HighScore}";
+            }
+            else
+            {
+                BestScoreText.text = "Best Score: none";
+            }
+        }
     }
 
     public void StartNew()
     {
         //NEW
-        if (MainDataPersistence.Instance != null)
+        if (MainDataPersistence.Instance != null && NameInput != null)
         {
             MainDataPersistence.Instance.PlayerName = NameInput.text;
         }
